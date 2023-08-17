0432fa4af30b15d82443ea72828880f3a397e613
a.alali@transtek.com <a.alali@transtek.com>
2023-08-17 11:58:55 +0300

    first commit from work

--- Frontend_WinForms/Form1.Designer.cs
+++ Frontend_WinForms/Form1.Designer.cs
@@ -28,19 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button2 = new Button();
             button1 = new Button();
             SuspendLayout();
-            // 
-            // button2
-            // 
-            button2.Location = new Point(21, 144);
-            button2.Name = "button2";
-            button2.Size = new Size(172, 26);
-            button2.TabIndex = 2;
-            button2.Text = "Change Text";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
             // 
             // button1
             // 
@@ -58,14 +47,12 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(button1);
-            Controls.Add(button2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-        private Button button2;
         private Button button1;
     }
 }
--- Frontend_WinForms/Form1.cs
+++ Frontend_WinForms/Form1.cs
@@ -16,50 +16,6 @@ namespace Frontend_WinForms
             InitializeComponent();
         }
 
-        private async void button2_Click(object sender, EventArgs e)
-        {
-            const string message_from_winForm = "I am the message from winform app";
-            //using (var client = new HttpClient())
-            //{
-            //    var content = new StringContent(message_from_winForm);
-            //    var response = await client.PostAsync("https://localhost:7088/DevzWeb/PostTestMessage", content);
-            //    var responseString = await response.Content.ReadAsStringAsync();
-            //    Console.WriteLine(responseString);
-            //}
-
-            // Create an HttpClient instance
-            HttpClient client = new HttpClient();
-
-            // Set the base address
-            client.BaseAddress = new Uri("https://localhost:7088/");
-
-            // Set the accept header to JSON
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            // Create a PageContent object with the message you want to send
-            PageContent message = new PageContent { Text = message_from_winForm,/* TextFormat = "Plain"*/ };
-
-            // Send a POST request to the endpoint
-            HttpResponseMessage response = await client.PostAsJsonAsync("DevzWeb/PostTestMessage", message);
-
-            // Check the response status code and read the content
-            if (response.IsSuccessStatusCode)
-            {
-                // Read the response as a PageContent object
-                string result = await response.Content.ReadAsAsync<string>();
-
-                // Do something with the result
-                Console.WriteLine(result);
-                //Console.WriteLine(result.text);
-                //Console.WriteLine(result.textFormat);
-            }
-            else
-            {
-                // Handle the error
-                Console.WriteLine(response.StatusCode);
-            }
-        }
-
         private async void button1_Click(object sender, EventArgs e)
         {
             const string message_from_winForm = "I am the message from winform app";
@@ -72,9 +28,6 @@ namespace Frontend_WinForms
             });
             await hubConnection.StartAsync();
             await hubConnection.InvokeAsync("SendMessage", message_from_winForm);
-
-
-
 
         }
     }
